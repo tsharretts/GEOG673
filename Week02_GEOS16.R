3e0e9a491ab9187a92e479a867990f2e0adfa1fb
Tyler Sharretts <71048993+tsharretts@users.noreply.github.com>
2020-10-09 19:11:50 -0400

    Add files via upload

---
+++ Week02_GEOS16.R
@@ -0,0 +1,25 @@
+### GOES-16 Sea-surface Satellite Data
+
+# load libraries
+library(ncdf4)
+library(fields)
+
+# loading the file
+ncFile <- ncdf4::nc_open("GEOG_673/geog473-673-master/geog473-673-master/datasets/OR_ABI-L2-SSTF-M3_G16_s20192081300453_e20192081400161_c20192081406297.nc")
+
+# load in the SST variable and save it as sstk
+sstK <- ncdf4::ncvar_get(nc=ncFile, varid="SST")
+
+# load in the latitudes and longitudes 
+lat <- ncdf4::ncvar_get(nc=ncFile, varid="latitude")
+lon <- ncdf4::ncvar_get(nc=ncFile, varid="longitude")
+
+# convert sst from Kelvin to Celsius
+sstC <- sstK - 273.15
+
+# remove values below 0C
+sstC[sstC < 0] = NA
+
+# Plot the matrix
+fields::image.plot(x=lon, y=lat, z=sstC, legend.lab="Celsius")
+title("GOES-R SST Product 7/25/2019 19:00 UTC")
