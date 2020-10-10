f11edca777c5d1306e77e60b39d488653a158875
Tyler Sharretts <71048993+tsharretts@users.noreply.github.com>
2020-10-10 18:52:54 -0400

    Add files via upload

---
+++ Week03_ReadingInCSV.R
@@ -0,0 +1,8 @@
+# Tyler Sharretts
+# Week 03: Time Series with R
+
+# DEOS data
+
+# Read in the csv file 
+DEOS_data <- read.csv("~/GEOG_673/geog473-673-master/geog473-673-master/datasets/DEOS.csv", header = TRUE, skip = 0, stringsAsFactors = FALSE)
+
